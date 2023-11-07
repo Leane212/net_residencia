c83b8369879322ecefd9c218e08f466ec7933f40
leanesoares <leanesoares212@gmail.com>
2023-11-07 17:41:24 -0300

    subindo tarefa

---
+++ semana1/Meuprojeto/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+#region TiposDados
+       int tipoInteiro;
+       double tipodouble;
+       decimal tipodecimal;
+       long tipoLong;
+
+       tipoInteiro = int.MaxValue;
+
+    Console.WriteLine($"Este valor {tipoInteiro} eh o maior inteiro possivel");
+
+#endregion
